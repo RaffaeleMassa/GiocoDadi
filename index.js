eb9abfc1f6cdec5b9cc171a939609516afd17098
RaffaeleMassa <76770497+RaffaeleMassa@users.noreply.github.com>
2021-02-12 17:32:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6)+ 1;
+var randomDiceImm = "dice" + randomNumber1 + ".png";
+var randomSourceImm = "images/" + randomDiceImm ;
+var imm = document.querySelectorAll("img")[0];
+imm.setAttribute("src", randomSourceImm);
+
+var randomNumber2= Math.floor(Math.random() * 6) +1;
+var randomSourceImm2 =  "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomSourceImm2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "O FTILL WINS";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = " A FTELL WINS";
+} else {
+    document.querySelector("h1").innerHTML = "DRAW"
+}
